23a1023e6e0cdd2ce117067f6ca7d4518ae6aedb
rabbitwave98 <167242625+rabbitwave98@users.noreply.github.com>
2026-03-28 18:15:12 -0700

    Quartz sync: Mar 28, 2026, 6:15 PM

--- content/index.md
+++ content/index.md
@@ -9,3 +9,5 @@ See the [documentation](https://quartz.jzhao.xyz) for how to get started.
 Testing. Testing. 123!
 
 Here's some more new text.
+
+This is after the last two CMD syncs.
--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "rabbitwave98.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
